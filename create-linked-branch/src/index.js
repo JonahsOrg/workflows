4bddf7afbdbd88afec053578d30da520ae14367e
Jonah Jackson <jonahjackson@uflip.com>
2023-10-09 15:50:37 -0500

    changed tab widths

--- create-linked-branch/src/index.js
+++ create-linked-branch/src/index.js
@@ -9,37 +9,37 @@ const github = require('@actions/github');
  * @returns {Promise<void>} Resolves when the action is complete.
  */
 async function run() {
- try {
-  /**
-   * We need to fetch all the inputs that were provided to our action
-   * and store them in variables for us to use.
-   **/
-  const token = core.getInput('token', { required: true });
-  const owner = core.getInput('owner', { required: true });
-  const repo = core.getInput('repo', { required: true });
-  const repoId = core.getInput('repo_id', { required: true });
-  const issueId = core.getInput('issue_id', { required: true });
-  const issueTitle = core.getInput('issue_title', { required: true });
-  const branchToCopy = core.getInput('branch_to_copy', { required: true });
+  try {
+    /**
+     * We need to fetch all the inputs that were provided to our action
+     * and store them in variables for us to use.
+     **/
+    const token = core.getInput('token', { required: true });
+    const owner = core.getInput('owner', { required: true });
+    const repo = core.getInput('repo', { required: true });
+    const repoId = core.getInput('repo_id', { required: true });
+    const issueId = core.getInput('issue_id', { required: true });
+    const issueTitle = core.getInput('issue_title', { required: true });
+    const branchToCopy = core.getInput('branch_to_copy', { required: true });
 
-  // this is the branch that is copied for each new issue branch when it is opened
-  // has to be formatted as refs/heads/<branch-name>
+    // this is the branch that is copied for each new issue branch when it is opened
+    // has to be formatted as refs/heads/<branch-name>
 
-  // need to change this to force branch names to fit a regex pattern
-  const branchToCopyRef = `refs/heads/${branchToCopy}`;
+    // need to change this to force branch names to fit a regex pattern
+    const branchToCopyRef = `refs/heads/${branchToCopy}`;
 
-  /**
-   * Now we need to create an instance of Octokit which will use to call
-   * GitHub's REST API endpoints.
-   * We will pass the token as an argument to the constructor. This token
-   * will be used to authenticate our requests.
-   * You can find all the information about how to use Octokit here:
-   * https://octokit.github.io/rest.js/v18
-   **/
-  const octokit = new github.getOctokit(token);
+    /**
+     * Now we need to create an instance of Octokit which will use to call
+     * GitHub's REST API endpoints.
+     * We will pass the token as an argument to the constructor. This token
+     * will be used to authenticate our requests.
+     * You can find all the information about how to use Octokit here:
+     * https://octokit.github.io/rest.js/v18
+     **/
+    const octokit = new github.getOctokit(token);
 
-  const { repository } = await octokit.graphql(
-   `
+    const { repository } = await octokit.graphql(
+      `
     query FetchLatestCommitOfBranch($owner: String!, $repo: String!, $branchName: String!) {
       repository(owner: $owner, name: $repo) {
         ref(qualifiedName: $branchName) {
@@ -58,19 +58,19 @@ async function run() {
       }
     }
   `,
-   {
-    owner,
-    repo,
-    branchName: branchToCopyRef
-   }
-  );
+      {
+        owner,
+        repo,
+        branchName: branchToCopyRef
+      }
+    );
 
-  const latestCommitSHA = repository?.ref?.target?.history?.edges[0]?.node?.oid;
-  if (!latestCommitSHA) return console.log('could not get the latestCommitSHA');
+    const latestCommitSHA = repository?.ref?.target?.history?.edges[0]?.node?.oid;
+    if (!latestCommitSHA) return console.log('could not get the latestCommitSHA');
 
-  const newBranchName = `refs/heads/${issueTitle.split(' ').join('-')}`;
-  await octokit.graphql(
-   `
+    const newBranchName = `refs/heads/${issueTitle.split(' ').join('-')}`;
+    await octokit.graphql(
+      `
       mutation CreateNewBranch($branch: String!, $sha: GitObjectID!, $assignToIssue: ID!, $repoId: ID!) {
         createLinkedBranch(
           input: {name: $branch, oid: $sha, repositoryId: $repoId, issueId: $assignToIssue}
@@ -79,20 +79,20 @@ async function run() {
         }
       }
       `,
-   {
-    repoId,
-    sha: latestCommitSHA,
-    branch: newBranchName,
-    assignToIssue: issueId
-   }
-  );
+      {
+        repoId,
+        sha: latestCommitSHA,
+        branch: newBranchName,
+        assignToIssue: issueId
+      }
+    );
 
-  console.log('successfully created the linked branch');
- } catch (error) {
-  console.log(error);
-  // Fail the workflow run if an error occurs
-  core.setFailed(error.message);
- }
+    console.log('successfully created the linked branch');
+  } catch (error) {
+    console.log(error);
+    // Fail the workflow run if an error occurs
+    core.setFailed(error.message);
+  }
 }
 
 run();
--- set-issue-label/src/index.js
+++ set-issue-label/src/index.js
@@ -9,30 +9,30 @@ const github = require('@actions/github');
  * @returns {Promise<void>} Resolves when the action is complete.
  */
 async function run() {
- try {
-  /**
-   * We need to fetch all the inputs that were provided to our action
-   * and store them in variables for us to use.
-   **/
-  const owner = core.getInput('owner', { required: true });
-  const repo = core.getInput('repo', { required: true });
-  const issueId = core.getInput('issue_id', { required: true });
-  const token = core.getInput('token', { required: true });
-  const label = core.getInput('label', { required: true });
+  try {
+    /**
+     * We need to fetch all the inputs that were provided to our action
+     * and store them in variables for us to use.
+     **/
+    const owner = core.getInput('owner', { required: true });
+    const repo = core.getInput('repo', { required: true });
+    const issueId = core.getInput('issue_id', { required: true });
+    const token = core.getInput('token', { required: true });
+    const label = core.getInput('label', { required: true });
 
-  /**
-   * Now we need to create an instance of Octokit which will use to call
-   * GitHub's REST API endpoints.
-   * We will pass the token as an argument to the constructor. This token
-   * will be used to authenticate our requests.
-   * You can find all the information about how to use Octokit here:
-   * https://octokit.github.io/rest.js/v18
-   **/
-  const octokit = new github.getOctokit(token);
+    /**
+     * Now we need to create an instance of Octokit which will use to call
+     * GitHub's REST API endpoints.
+     * We will pass the token as an argument to the constructor. This token
+     * will be used to authenticate our requests.
+     * You can find all the information about how to use Octokit here:
+     * https://octokit.github.io/rest.js/v18
+     **/
+    const octokit = new github.getOctokit(token);
 
-  // fetch the ids of the parsed label and issue number
-  const { repository } = await octokit.graphql(
-   `
+    // fetch the ids of the parsed label and issue number
+    const { repository } = await octokit.graphql(
+      `
     query FetchLabelAndIssueIds($owner: String!, $repo: String!, $labelName: String!) {
       repository(owner: $owner, name: $repo) {
         label(name: $labelName) {
@@ -41,39 +41,39 @@ async function run() {
       }
     }
   `,
-   {
-    owner,
-    repo,
-    labelName: label
-   }
-  );
+      {
+        owner,
+        repo,
+        labelName: label
+      }
+    );
 
-  if (!repository) return;
+    if (!repository) return;
 
-  // grab the ids
-  const labelId = repository?.label?.id;
-  if (!labelId || !issueId) return;
+    // grab the ids
+    const labelId = repository?.label?.id;
+    if (!labelId || !issueId) return;
 
-  // labels the issue
-  await octokit.graphql(
-   `
+    // labels the issue
+    await octokit.graphql(
+      `
     mutation SetLabelOnIssue( $issueId: ID!, $labelId: ID! )  {
       addLabelsToLabelable(input: {labelableId: $issueId, labelIds: [$labelId]}) {
         clientMutationId
       }
     }
     `,
-   {
-    issueId,
-    labelId
-   }
-  );
+      {
+        issueId,
+        labelId
+      }
+    );
 
-  console.log('successfully labeled the issue');
- } catch (error) {
-  // Fail the workflow run if an error occurs
-  core.setFailed(error.message);
- }
+    console.log('successfully labeled the issue');
+  } catch (error) {
+    // Fail the workflow run if an error occurs
+    core.setFailed(error.message);
+  }
 }
 
 run();
