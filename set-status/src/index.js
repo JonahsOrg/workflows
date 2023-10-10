546533f14012cb73c61fa65bc9dae9b38b280d0b
Jonah Jackson <jonahjackson@uflip.com>
2023-10-10 15:51:06 -0500

    testing set status outputs

--- set-status/src/index.js
+++ set-status/src/index.js
@@ -14,9 +14,10 @@ async function run() {
      * We need to fetch all the inputs that were provided to our action
      * and store them in variables for us to use.
      **/
-    const owner = core.getInput('owner', { required: true });
-    const repo = core.getInput('repo', { required: true });
+    const issueId = core.getInput('issue_id', { required: true });
     const token = core.getInput('token', { required: true });
+
+    const statusName = 'In Development';
 
     /**
      * Now we need to create an instance of Octokit which will use to call
@@ -29,52 +30,79 @@ async function run() {
     const octokit = new github.getOctokit(token);
 
     // fetch the ids of the parsed label and issue number
-    const { repository } = await octokit.graphql(
+    const { node } = await octokit.graphql(
       `
-        query FetchLabelAndIssueIds($owner: String!, $repo: String!, $labelName: String!) {
-          repository(owner: $owner, name: $repo) {
-            issue(number: $issueNumber) {
-              id # issue id
-              projectItems(first: 1) {
-                nodes {
-                  id # card id
-                  project {
-                    id # project id
+      query FetchIds($issueId: ID!, $statusName: String!) {
+        node(id: $issueId) {
+          ... on Issue {
+            projectItems(first: 10) {
+              nodes {
+                id # card id
+                project {
+                  id # project id
+                }
+                ... on ProjectV2Item {
+                  fieldValueByName(name: "Status") {
+                    ... on ProjectV2ItemFieldSingleSelectValue {
+                      field {
+                        ... on ProjectV2SingleSelectField {
+                          id # id of the field
+                          options (names: [$statusName]) {
+                            id  # id of specified field
+                          }
+                        }
+                      }
+                    }
                   }
                 }
               }
             }
           }
         }
+      }
       `,
       {
-        owner,
-        repo
+        issueId,
+        statusName
       }
     );
 
-    if (!repository) return;
+    if (!node) return;
 
     // grab the ids
-    const labelId = repository?.label?.id;
-    if (!labelId || !issueId) return;
+    const cardId = node?.projectItems[0]?.node?.id;
+    const projectId = node?.projectItems[0]?.node?.project?.id;
 
-    // labels the issue
-    await octokit.graphql(
-      `
-    mutation SetLabelOnIssue( $issueId: ID!, $labelId: ID! )  {
-      addLabelsToLabelable(input: {labelableId: $issueId, labelIds: [$labelId]}) {
-        clientMutationId
-      }
-    }
-    `,
-      {
-        issueId,
-        labelId
-      }
-    );
+    console.log(node?.projectItems[0]?.node);
 
-    console.log('successfully labeled the issue');
+    if (!cardId || !projectId) return;
+
+    console.log({ cardId, projectId });
+
+    // const tempPayload = { singleSelectOptionId: '4b2fdd91' };
+
+    // changes the card status in the project
+    // await octokit.graphql(
+    //   `
+    //   mutation UpdateStatusOfProjCard($item: ID!, $project: ID!, $field: ID!, $payload: ProjectV2FieldValue!) {
+    //     updateProjectV2ItemFieldValue(
+    //       input: {projectId: $project, itemId: $item, fieldId: $field, value: $payload}
+    //     ) {
+    //       projectV2Item {
+    //         id
+    //       }
+    //     }
+    //   }
+    //   `,
+    //   {
+    //     field: fieldId || 'PVTSSF_lAHOBk645c4AVbXfzgNsVfc',
+    //     item: projectCardId || 'PVTI_lAHOBk645c4AVbXfzgJgFoE',
+    //     project: projectId || 'PVT_kwHOBk645c4AVbXf',
+    //     payload: payloadObj || tempPayload
+    //   }
+    // );
+
+    console.log('successfully updated the status in the project');
   } catch (error) {
     // Fail the workflow run if an error occurs
     core.setFailed(error.message);
